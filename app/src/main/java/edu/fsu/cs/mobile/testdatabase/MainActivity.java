65f9b828913a0774041d27e0ca433e78b6569557
Hung-Tan-Tran <HTT15@my.fsu.edu>
2018-10-19 10:28:28 -0400

    added splash screen

--- app/src/main/java/edu/fsu/cs/mobile/testdatabase/MainActivity.java
+++ app/src/main/java/edu/fsu/cs/mobile/testdatabase/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity implements SetNameAdapter.It
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
